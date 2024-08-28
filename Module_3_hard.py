f5fa60674a8cc47910a4d8859745618e75e4f12a
Yukie765 <yliya-kate@mail.ru>
2024-08-28 13:48:27 +0500

    Add files via upload

---
+++ Module_3_hard.py
@@ -0,0 +1,34 @@
+types = {list, tuple, set}
+
+def calculate_structure_sum(args):
+    res = 0
+    for i in args:
+        if type(i) in types:
+            res += calculate_structure_sum(i)
+        else:
+            if isinstance(i, int) or isinstance(i, float):
+                res += i
+            if isinstance(i, str):
+                res += len(i)
+            if isinstance(i, dict):
+                for key,val in i.items():
+                    if type(key) == int:
+                        res += key
+                    else:
+                        res += len(key)
+                    if type(val) == int:
+                        res += val
+                    else:
+                        res += len(val)
+    return res
+
+
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+print(calculate_structure_sum(data_structure))
